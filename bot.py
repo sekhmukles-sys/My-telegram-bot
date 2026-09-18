61147a8db0a2efff2c5ff3369791c32d9f1c4266
sekhmukles-sys <sekhmukles@gmail.com>
2026-09-18 17:07:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from telegram.ext import Application, CommandHandler
 TOKEN = '8067725589:AAHaOtDiBu8uwk9emqmxrprG0lyXJGiTWOM'
 app = Flask(name)
 async def start(update, context):
+  
 await update.message.reply_text('Hello. This is a webhook bot.')
 async def main():
 app_builder = Application.builder().token(TOKEN).build()
